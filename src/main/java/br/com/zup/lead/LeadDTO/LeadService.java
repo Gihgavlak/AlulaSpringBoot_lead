ca80f66e5546cf4ccf423a1889ecee9191c7e648
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-10-29 16:06:39 -0300

    Criar metodo para validar e-mail

--- src/main/java/br/com/zup/lead/LeadDTO/LeadService.java
+++ src/main/java/br/com/zup/lead/LeadDTO/LeadService.java
@@ -1,6 +1,8 @@
 package br.com.zup.lead.LeadDTO;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,4 +18,15 @@ public class LeadService {
     public List<LeadDTO> retornarTodosOsLead(){
         return listaDeLead;
     }
+
+    public void validarEmail(LeadDTO leadDTO){
+        for (LeadDTO referencia: listaDeLead) {
+            if (referencia.getEmail().equalsIgnoreCase(leadDTO.getEmail())){
+                throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+            }
+        }
+        listaDeLead.add(leadDTO);
+
+    }
+
 }
