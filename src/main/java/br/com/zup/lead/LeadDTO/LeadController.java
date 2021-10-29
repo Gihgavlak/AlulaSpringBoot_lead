16234fe6e1d5b32949105dd398cbad216fb64468
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-10-29 15:02:14 -0300

    Criar método cadastrar para lead no Post

--- src/main/java/br/com/zup/lead/LeadDTO/LeadController.java
+++ src/main/java/br/com/zup/lead/LeadDTO/LeadController.java
@@ -1,4 +1,19 @@
 package br.com.zup.lead.LeadDTO;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/leads")
 public class LeadController {
+    @Autowired
+    private LeadService leadService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void cadastrarLead(@RequestBody LeadDTO leadDTO){
+
+    }
+
 }
